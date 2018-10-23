ae0d2ae8a15e88c70941e9f5fc8272578f3007c3
Admin <ngocanhvu18ict@gmail.com>
2018-10-23 16:31:23 +0700

    init

---
+++ AddEditDelete/TableViewController.swift
@@ -0,0 +1,101 @@
+//
+//  TableViewController.swift
+//  AddEditDelete
+//
+//  Created by Admin on 10/23/18.
+//  Copyright © 2018 Admin. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+    var array = Array(1...10)
+    @IBOutlet weak var addButton: UIBarButtonItem!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+        
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return array.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = String(array[indexPath.row])
+        // Configure the cell...
+
+        return cell
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            array.remove(at: indexPath.row)
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+   
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let data = segue.destination as? ViewController {
+            if let index = tableView.indexPathForSelectedRow {
+                data.name = String(array[index.row])
+            }
+        }
+    }
+   
+    @IBAction func unwind(for unwindSegue: UIStoryboardSegue) {
+        if let passData = unwindSegue.source as? ViewController {
+            if let indexPath = tableView.indexPathForSelectedRow {
+                array[indexPath.row] = Int(passData.name ?? "") ?? 0
+            }else {
+                array.append(Int(passData.name ?? "") ?? 0)
+            }
+        }
+        tableView.reloadData()
+    }
+}
--- AddEditDelete/ViewController.swift
+++ AddEditDelete/ViewController.swift
@@ -9,17 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var name: String?
+    
+    @IBOutlet weak var saveButton: UIButton!
+    @IBOutlet weak var textField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        if name != nil {
+            textField.text = name
+        }
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
 
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard name != "" else {
+            return
+        }
+        name = textField.text
+    }
+ 
 
 }
-
